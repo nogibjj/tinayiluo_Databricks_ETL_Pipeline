15fb94df37b873e68d057ca7747336aa62e0b399
tina.yi@duke.edu <tina.yi@duke.edu>
2023-11-12 22:27:58 +0000

    update query format

--- Notebook_Folder/query_viz.py
+++ Notebook_Folder/query_viz.py
@@ -1,8 +1,4 @@
 # Databricks notebook source
-"""
-query and viz file
-"""
-
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
 
@@ -26,8 +22,10 @@ def query_transform():
         "b.fatalities_85_99, "
         "b.fatalities_00_14, "
         "(a.incidents_85_99 + b.incidents_00_14) AS total_incidents, "
-        "a.fatal_accidents_85_99 + b.fatal_accidents_00_14 AS total_fatal_accidents, "
-        "b.fatalities_85_99 + b.fatalities_00_14 AS total_fatalities "
+        "a.fatal_accidents_85_99 + b.fatal_accidents_00_14 "
+        "AS total_fatal_accidents, "
+        "b.fatalities_85_99 + b.fatalities_00_14 "
+        "AS total_fatalities "
         "FROM "
         "airline_safety1_delta AS a "
         "JOIN "
@@ -37,8 +35,10 @@ def query_transform():
         "LIMIT 10"
     )
 
+
     query_result = spark.sql(query)
     return query_result
+
 
 # sample viz for project
 def viz():
@@ -54,32 +54,14 @@ def viz():
 
     # Bar Plot showing total Incidents vs Total Fatalities for all the Airlines (1985-2014)
     plt.figure(figsize=(15, 7))
-    query_result_pd.plot(x='airline', y=['total_incidents', 'total_fatal_accidents', 'total_fatalities'], kind='bar')
-    plt.title('Total Incidents vs. Fatal Accidents vs. Total Fatalities for Each Airline (1985-2014)')
+    query_result_pd.plot(x='airline', y=['total_incidents', 'total_fatal_accidents', 
+                                         'total_fatalities'], kind='bar')
+    plot_title = ('Total Incidents vs. Fatal Accidents vs. '
+                  'Total Fatalities for Each Airline (1985-2014)')
+    plt.title(plot_title)
     plt.ylabel('Counts')
     plt.xlabel('Airline')
     plt.xticks(rotation=45)
-    plt.legend(title='Metrics')
-    plt.tight_layout()
-    plt.show()
-
-    # Bar Plot showing total Incidents vs. Total Fatalities for selected Airline (1985-2014)
-    # Filter for the specific airline
-    selected_airline = 'Air France'  
-    airline_data = query_result_pd[query_result_pd['airline'] == selected_airline]
-
-    # Check if the filtered DataFrame is empty
-    if airline_data.empty:
-        print(f"No data available for {selected_airline}.")
-        return
-
-    # Bar Plot showing total Incidents vs. Total Fatalities for the selected Airline (1985-2014)
-    plt.figure(figsize=(10, 6))
-    airline_data.plot(x='airline', y=['total_incidents', 'total_fatal_accidents', 'total_fatalities'], kind='bar')
-    plt.title(f'Total Incidents vs. Fatal Accidents vs. Total Fatalities for {selected_airline} (1985-2014)')
-    plt.ylabel('Counts')
-    plt.xlabel('Airline')
-    plt.xticks(rotation=0)  # No need to rotate for a single airline
     plt.legend(title='Metrics')
     plt.tight_layout()
     plt.show()
@@ -96,7 +78,8 @@ def viz():
         plt.plot(periods, fatalities, marker='o', label=row['airline'])
 
     # Customize the plot
-    plt.title('Total Fatalities Change for Each Airline (1985-1999 vs 2000-2014)')
+    plt.title('Total Fatalities Change for Each Airline '
+              '(1985-1999 vs 2000-2014)')
     plt.ylabel('Number of Fatalities')
     plt.xlabel('Time Period')
     plt.legend(title='Airlines', bbox_to_anchor=(1.05, 1), loc='upper left')
@@ -105,25 +88,6 @@ def viz():
 
     # Show the plot
     plt.show()
-    
-    # Select a specific airline 
-    selected_airline = 'Air France'  
-    airline_data = query_result_pd[query_result_pd['airline'] == selected_airline]
-
-    # Prepare data for plotting
-    periods = ['1985-1999', '2000-2014']
-    fatalities = [airline_data['fatalities_85_99'].values[0], airline_data['fatalities_00_14'].values[0]]
-
-    # Plotting
-    plt.figure(figsize=(8, 5))
-    plt.plot(periods, fatalities, marker='o')
-    plt.title(f'Total Fatalities Change for {selected_airline} (1985-1999 vs 2000-2014)')
-    plt.ylabel('Number of Fatalities')
-    plt.xlabel('Time Period')
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
 
 if __name__ == "__main__":
     query_transform()
